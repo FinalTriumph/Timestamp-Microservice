d5225684619c74db2a4e2d920b0663ca278654b6
Kaspars Andzans <kaspars_andzans@inbox.lv>
2016-11-21 19:10:59 +0000

    Almost ready

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+
+function natural(unix){
+    var date = new Date(unix * 1000);
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var month = months[date.getMonth()];
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var result = month + " " + day + ", " + year;
+    return result;
+}
+
+app.get("/", function(req, res){
+   res.sendFile(__dirname + "/index.html"); 
+});
+
+app.get("/:time", function(req, res){
+   if (!isNaN(req.params.time)){
+       var nat = natural(req.params.time);
+       var full = {unix: Number(req.params.time), natural: nat};
+       res.json(full);
+   } else {
+       var timeUn = new Date(req.params.time);
+       if (!isNaN(timeUn)){
+           res.json({unix: (timeUn / 1000), natural: req.params.time});
+       } else {
+           res.json({unix: null, natural: null});
+       }
+   }
+});
+
+app.listen(8080, function(){
+    console.log("Listening on port 8080!");
+});
